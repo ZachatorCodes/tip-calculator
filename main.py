d70841bfc9a492a4c9710247b8e9b74c36d36b29
ZachatorCodes <104332807+ZachatorCodes@users.noreply.github.com>
2024-02-20 02:08:07 +0000

    Reformat code

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ print("Welcome to Zach's tip calculator!")
 
 # Input Constants
 total_bill = float(input("What was the total bill? ($)\n"))
-tip_percent = 1 + (float(input("What percent would you like to tip?\n")) / 100)
+tip_percent = float(input("What percent would you like to tip?\n")) / 100
 people_paying = int(input("How many people are splitting the bill?\n"))
 
-bill_and_tip = total_bill * tip_percent
+bill_and_tip = total_bill * (1 + tip_percent)
 bill_per_person = round(bill_and_tip / people_paying, 2)
 
 print(f"Each person should pay ${bill_per_person}")
